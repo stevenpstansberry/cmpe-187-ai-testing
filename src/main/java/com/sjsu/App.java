f3e95f6555b91ac803a9561edf0b5bb4eede2272
Steven <stevenpstansberry@gmail.com>
2024-04-29 13:18:25 -0700

    maven init

---
+++ src/main/java/com/sjsu/App.java
@@ -0,0 +1,13 @@
+package com.sjsu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
